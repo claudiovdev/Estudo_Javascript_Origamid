09a12e387e347e52e88fb3aa17088cf5c606a0d4
claudiovdev <c.viniciusmagalhaes@gmail.com>
2025-08-28 15:53:29 -0300

    Tipos de variáveis

---
+++ Javascript_para_iniciantes/Tipos_de_dados/script.js
@@ -0,0 +1,50 @@
+var nome = 'Claudio';
+var idade = 36;
+var simbolo = Symbol();
+
+//console.log(typeof simbolo);
+
+var nome = 'Vinicius';
+var sobreNome = 'Magalhães';
+var nomeCompleto = nome + ' ' + sobreNome;
+//console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romário fez  ' + gols + ' gols';
+//console.log(frase);
+
+var ano = '2018';
+var mes = 8;
+//console.log(ano + mes);
+
+var frase1 = "Esse é o \"melhor\" jogo";
+//console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols} gols`
+//console.log(frase2)
+
+// DESAFIO ------------------
+
+
+// Declare uma variável contendo uma string
+var cor = 'Vermelho';
+
+// Declare uma variável contendo um número dentro de uma string
+var numero = '200';
+
+// Declare uma variável com a sua idade
+var idade = 36;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Claudio', sobreNome = 'Vinicius';
+var nomeCompleto = `${nome} ${sobreNome}`
+console.log(nomeCompleto)
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
