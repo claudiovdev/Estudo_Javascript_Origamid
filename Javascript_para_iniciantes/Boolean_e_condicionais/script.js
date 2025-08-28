793ee66339e0bbf6e88327bacdc33d9b7c9890fb
claudiovdev <c.viniciusmagalhaes@gmail.com>
2025-08-28 16:18:36 -0300

    Boolean e condicionais

---
+++ Javascript_para_iniciantes/Boolean_e_condicionais/script.js
@@ -0,0 +1,29 @@
+var possueGraduacao = false;
+var possueDoutorado = false;
+var x;
+
+if(possueGraduacao){
+  console.log('Possue graduação')
+  x = 10;
+}else if(possueDoutorado){
+  console.log('Possue doutorado')
+  x = 5;
+}else{
+  console.log('Não possue nada')
+  x = 0;
+}
+
+console.log(x)
+
+var nome = 'Claudio';
+
+if(nome){
+  console.log(nome)
+}else{
+  console.log('Nome não existe')
+}
+
+var y = '10';
+
+console.log(y === 10);
+
---
+++ Javascript_para_iniciantes/Numeros_e_operadores/script.js
@@ -0,0 +1,45 @@
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+
+
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2 // NaN (Not a Number)
+
+
+var incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+var incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+
+var frase = 'Isso é um teste';
++frase; // NaN
+-frase; // NaN
+
+var idade = '28';
++idade; // 28 (número)
+-idade; // -28 (número)
+console.log(+idade + 5); // 33 
+
+var possuiFaculdade = true;
+console.log(+possuiFaculdade); // 1
