3fc8cc00dbdf51266f08f3b68f88b4b9c6095d79
claudiovdev <c.viniciusmagalhaes@gmail.com>
2025-09-02 14:45:50 -0300

    Objetos e String

---
+++ Objetos/String/script.js
@@ -0,0 +1,44 @@
+const comida = 'Pizza';
+const agua = new String('Agua');
+
+comida.length;
+frames.length;
+
+console.log(agua.length)
+
+const frase = 'A melhor comida';
+
+console.log(frase[frase.length -1])
+console.log(frase.charAt(frase.length -3))
+
+const frase2 = 'A melhor lingaguem é ';
+const lingaguem = 'JavaScript';
+
+const fraseFinal = frase2.concat(lingaguem);
+
+console.log(fraseFinal);
+
+const fruta = 'Banana';
+const listaFrutas = 'Melancia, Banana, Laranja';
+
+console.log(listaFrutas.includes(fruta));
+
+console.log(fruta.startsWith('Ba'))
+
+console.log(fruta.endsWith('na'))
+
+console.log(fruta)
+
+const transacao1 = 'Deposito do cliente';
+const transacao2 = 'Deposito do fornecedor';
+const transacao3 = 'Taxa de camisas';
+
+console.log(transacao1.slice(0,5))
+console.log(transacao1.slice(-7))
+console.log(transacao1.slice(0,-2))
+
+console.log(fruta.indexOf('a'))
+console.log(fruta.lasIndexOf('a'))
+
+
+
