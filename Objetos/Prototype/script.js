72aba887dd2d0d957098dab05b9b11286cad56ac
claudiovdev <c.viniciusmagalhaes@gmail.com>
2025-09-02 11:02:25 -0300

    Prototype

---
+++ Objetos/Prototype/script.js
@@ -0,0 +1,12 @@
+const pais = 'Brasil';
+const cidade = new String('Rio');
+
+const listaAnimais = ['Cachorro', 'Gato', 'Cavalo']
+
+const Carro = {
+  marca: 'Ford',
+  preco: 2000,
+  andar(){
+    return true;
+  }
+}
